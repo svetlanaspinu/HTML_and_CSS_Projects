be5c593ef508804a9772dbc62fe7f7dac8ec0fcd
svetlanaspinu <s.spinu@yahoo.com>
2023-06-21 13:09:09 +0100

    Basic HTML and JavaScript 1 Assignment

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,6 +1,2 @@
-<!DOCTYPE HTML>
-<html>
-<script>
+
 alert("Hello, World!");
-</script>
-</html>
---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_2.js
@@ -0,0 +1,6 @@
+
+    function My_First_Function() {
+        var String = "I love all the flowers!!";
+        var result = String.fontcolor("blue");
+        document.getElementById("Flowers").innerHTML = result;
+    }
